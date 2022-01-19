41122cc6f6a4d44f441495acc6f5d5af01332c84
iamliormalichi <malichispam@gmail.com>
2022-01-19 14:15:19 +0200

    improved reco section

--- js/app.js
+++ js/app.js
@@ -1,21 +1,49 @@
 const card1 = document.querySelector(".card__inner1")
 const card2 = document.querySelector(".card__inner2")
 const card3 = document.querySelector(".card__inner3")
+
+const reco__innerA1 = document.querySelector(".reco__innerA1")
+const reco__innerA2 = document.querySelector(".reco__innerA2")
+const reco__innerA3 = document.querySelector(".reco__innerA3")
+
 const recoCard1 = document.querySelector(".reco-inner1")
 const recoCard2 = document.querySelector(".reco-inner2")
 const recoCard3 = document.querySelector(".reco-inner3")
 const recoCard4 = document.querySelector(".reco-inner4")
 const recoCard5 = document.querySelector(".reco-inner5")
+const clickMeCard1 = document.querySelector(".clickMeCard1")
+const clickMeCard2 = document.querySelector(".clickMeCard2")
+const clickMeCard3 = document.querySelector(".clickMeCard3")
+
+const clickMeReco1 = document.querySelector(".clickMeReco1")
+const clickMeReco2 = document.querySelector(".clickMeReco2")
+const clickMeReco3 = document.querySelector(".clickMeReco3")
+
 
 card1.addEventListener("click", function(){
+    clickMeCard1.innerHTML = ""
     card1.classList.toggle("is-flipped1");
 });
 
 card2.addEventListener("click", function(){
+    clickMeCard2.innerHTML = ""
     card2.classList.toggle("is-flipped2");
 });
-card3.addEventListener("click", function(){
+card3.addEventListener("click", function(){    
+    clickMeCard3.innerHTML = ""
     card3.classList.toggle("is-flipped3");
+});
+
+
+reco__innerA1.addEventListener("click", function(){    
+    clickMeReco1.innerHTML = ""
+    reco__innerA1.classList.toggle("is-flipped3");
+});reco__innerA2.addEventListener("click", function(){    
+    clickMeReco2.innerHTML = ""
+    reco__innerA2.classList.toggle("is-flipped3");
+});reco__innerA3.addEventListener("click", function(){    
+    clickMeReco3.innerHTML = ""
+    reco__innerA3.classList.toggle("is-flipped3");
 });
 
 recoCard1.addEventListener("click", function(){recoCard1.classList.toggle("reco-is-flipped1")})
